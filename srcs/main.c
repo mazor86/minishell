b43befc93b7f2d4b689d63d2ce1105e7fc461b91
Alexandr Dowgij <jlyessa@at-h5.msk.21-school.ru>
2020-10-26 13:16:31 +0300

    1

--- srcs/main.c
+++ srcs/main.c
@@ -6,17 +6,19 @@
 /*   By: jlyessa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 13:35:15 by jlyessa           #+#    #+#             */
-/*   Updated: 2020/10/26 13:11:45 by jlyessa          ###   ########.fr       */
+/*   Updated: 2020/10/26 13:16:10 by jlyessa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishel.h"
 
-int		main()
+int		main(int args, char **argv, char **env)
 {
 	char	buf[BUFFER_SIZE + 1];
 	int		ret;
 	
+	(void)args;
+	(void)argv;
 	while ((ret = read(0, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
