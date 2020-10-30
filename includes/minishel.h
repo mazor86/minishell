ecb9290b747c802e3662ef0649467143c2d0e984
jlyessa <jlyessa@student.21-school.ru>
2020-10-30 16:30:37 +0300

    1

--- includes/minishel.h
+++ includes/minishel.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishel.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tisabel <tisabel@student.21-school.ru>     +#+  +:+       +#+        */
+/*   By: jlyessa <jlyessa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 21:35:01 by tisabel           #+#    #+#             */
-/*   Updated: 2020/10/30 19:06:31 by tisabel          ###   ########.fr       */
+/*   Updated: 2020/10/30 16:30:00 by jlyessa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,6 @@
 
 # define BUFFER_SIZE 1024
 
-typedef struct	s_all
-{
-	
-}				t_all;
-
 typedef struct		s_data
 {
 	char			*name;
@@ -43,13 +38,13 @@ typedef struct		s_list
 {
 	t_data			*content;
 	struct s_list	*next;
-	
 }					t_list;
-	
 
 int				get_next_line(int fd, char **line);
 int				parce_line(t_list **command, char *line);
 char			*ft_strcut(char *str, char c);
 void			init_data(t_data *data);
+
+int				start_cmd(t_list *lst);
 
 #endif
