dd3271d0a5c3200d663cb508a881f5172ff64654
Julia Martsenko <jmartsenko@gmil.com>
2020-12-01 17:15:02 +0700

    variable structure

--- includes/struct.h
+++ includes/struct.h
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   struct.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tisabel <tisabel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tisabel <tisabel@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 13:38:46 by tisabel           #+#    #+#             */
-/*   Updated: 2020/11/09 19:55:16 by tisabel          ###   ########.fr       */
+/*   Updated: 2020/11/30 22:56:11 by tisabel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCT_H
 # define STRUCT_H
+
 /*
 ** name = name of the command
 ** flag = if there are any flags in the command
@@ -19,6 +20,12 @@
 ** tail = if there is something left in the read line
 ** 
 */
+
+typedef struct		s_var
+{
+	char			*name;
+	char			*value;
+}					t_var;
 
 typedef struct		s_data
 {
